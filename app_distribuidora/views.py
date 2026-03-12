b520f40b9bf5abb6793c16e61bb080dacee32ee1
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-03-12 15:45:07 -0300

    feat: criando sessão início e produtos

--- app_distribuidora/views.py
+++ app_distribuidora/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     """Página principal"""
     return render(request, 'app_distribuidora/index.html')
+
+def base(request):
+    """Barra de navegação e rodapé da página"""
+    return render(request, 'app_distribuidora/base.html')
