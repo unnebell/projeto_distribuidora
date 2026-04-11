03a6d5a70cb0d87858906d8c9fe3ac83437aae5f
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-04-11 10:09:03 -0300

    feat: add produtos fakes para teste for no index

--- app_distribuidora/views.py
+++ app_distribuidora/views.py
@@ -1,3 +1,5 @@
+from urllib import request
+
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -7,6 +9,18 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 
 def index(request):
     """Página principal"""
+    produtos = [
+        {'id': 1, 'nome': 'Notebook', 'quantidade': 3, 'descricao': 'Ideapad 3I'},
+        {'id': 2, 'nome': 'Mouse', 'quantidade': 10, 'descricao': 'Logitech G305'},
+        {'id': 3, 'nome': 'Teclado', 'quantidade': 5, 'descricao': 'Mecânico RGB'},
+        {'id': 4, 'nome': 'Notebook', 'quantidade': 3, 'descricao': 'Ideapad 3I'},
+        {'id': 5, 'nome': 'Mouse', 'quantidade': 10, 'descricao': 'Logitech G305'},
+        {'id': 6, 'nome': 'Teclado', 'quantidade': 5, 'descricao': 'Mecânico RGB'},
+    ]
+    
+    
+    return render(request, 'app_distribuidora/index.html', {'produtos':produtos})
+
     return render(request, 'app_distribuidora/index.html')
 
 def base(request):
