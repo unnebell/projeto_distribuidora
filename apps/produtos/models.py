f6d58df7ec314378f3696cca60871633d40c8403
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-04-15 16:37:49 -0300

    feat: criando bando de dados para produtos

--- apps/produtos/models.py
+++ apps/produtos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+    
+admin.site.register(Produto)
