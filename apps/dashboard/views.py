71cd78c158db50aba5a4fc3df9d2e1b898fe5d90
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-04-15 22:53:22 -0300

    feat: add dados reais do bd ao inves de fakes

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
 
+from apps.produtos.models import Produto
+
 # Create your views here.
 @login_required(login_url='/auth/login/') # Bloqueio de acesso para usuários não autenticados - redirecionamento para página de login
 @user_passes_test(lambda u: u.is_staff, login_url='/auth/login') # Bloqueio de clientes para página adm - acesso somente de administrador
 def dashboard(request):
-    produtos = [
-        {'id': 1, 'nome': 'Notebook', 'quantidade': 3, 'descricao': 'Ideapad 3I'},
-        {'id': 2, 'nome': 'Mouse', 'quantidade': 10, 'descricao': 'Logitech G305'},
-        {'id': 3, 'nome': 'Teclado', 'quantidade': 5, 'descricao': 'Mecânico RGB'},
-    ]
-    
-    
-    return render(request, 'dashboard/painel.html', {'produtos':produtos})
+    produtos = Produto.objects.all()
+
+    if request.method == 'GET':
+        return render(request, 'dashboard/painel.html', {'produtos':produtos})
