6f9769efd022022f2d567f7b15d158e335050000
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-05-04 09:43:13 -0300

    update: pesquisa independente de acentuação

--- apps/produtos/views.py
+++ apps/produtos/views.py
@@ -1,17 +1,24 @@
+import unicodedata
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Produto
 
+def remover_acentos(texto):
+    nfkd = unicodedata.normalize('NFKD', texto) #Separa cada letra para checar se tem acentuação
+    return ''.join(c for c in nfkd if not unicodedata.combining(c)).lower() #Descarta acento e junta as letras novamente
+
 def produtos(request):
-    query = request.GET.get('q', '').strip()
+    query = request.GET.get('q', '').strip() #Pega o texto digitado para fazer a busca
     qs = Produto.objects.filter(ativo=True)
-    
+
     if query:
-        qs = qs.filter(nome__icontains=query)
-    
-    paginator = Paginator(qs, 12)
+        query_limpo = remover_acentos(query)
+        
+        qs = [p for p in qs if query_limpo in remover_acentos(p.nome)] #Percorre cada produto, retorna somente se acha algo igual
+
+    paginator = Paginator(qs, 12) #Divide cada página com 12 produtos
     page_obj = paginator.get_page(request.GET.get('page'))
-    
+
     return render(request, 'produtos/produtos.html', {
         'produtos': page_obj,
         'query': query,
